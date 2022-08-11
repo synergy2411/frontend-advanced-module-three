98d37b82097cbc4b8be975e362fd416ca1260909
Sumit K <synergy2411@outlook.com>
2022-08-11 14:25:47 +0530

    babel plugin added

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"]
+            }, {
+                test: /\.js$/i,
+                use: "babel-loader",
+                exclude: /node_modules/
             }
         ]
     },
